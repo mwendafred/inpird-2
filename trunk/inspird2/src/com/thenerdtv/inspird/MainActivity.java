65e4e2547a2642a9147b446dcd6ee0434cf731fb
charleszink@gmail.com <charleszink@gmail.com@c10a482d-f8c7-f030-f3bb-d33c6b7da834>
2010-04-08 07:17:22 +0000

    Added main code. Things left to do: Logo Tab Icons Add Quotes About Page

--- trunk/inspird2/src/com/thenerdtv/inspird/MainActivity.java
+++ trunk/inspird2/src/com/thenerdtv/inspird/MainActivity.java
@@ -1,13 +1,29 @@
+/** Define our package */
 package com.thenerdtv.inspird;
 
-import android.app.Activity;
+/** Import required packages */
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends TabActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TabHost tabHost = getTabHost();
+
+        LayoutInflater.from(this).inflate(R.layout.main, tabHost.getTabContentView(), true);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("Quotes")
+                .setContent(R.id.quotes));
+        tabHost.addTab(tabHost.newTabSpec("tab2")
+                .setIndicator("Bible Verses")
+                .setContent(R.id.bible));
+        tabHost.addTab(tabHost.newTabSpec("tab3")
+                .setIndicator("About")
+                .setContent(R.id.about));
     }
 }
